0ff6ed9d95ccd0dfcc90d3749e2a50d942b8e8f8
SAHIL RAJA <sraza1692@gmail.com>
2025-09-02 19:15:18 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Conditional{
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = 1;
+    int b = 3;
+    int c = 6;
+    if(a>b && a>c){
+        System.out.println("A is greater");
+         }
+     else if (b>a && b>c){
+        System.out.println("B is greater");
+         }
+    else{
+        System.out.println("C is Greater");
+        }
+       
+    }
+}
